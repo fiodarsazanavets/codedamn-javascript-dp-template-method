57410990b9be49e06db9df0cf89deb5cbee8cdb5
Fiodar Sazanavets <33926695+fiodarsazanavets@users.noreply.github.com>
2023-03-03 19:46:52 +0800

    Refactor for exercises.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const BasicTextToHtmlConverter = require('./basicTextToHtmlConverter.js');
 const MdToHtmlConverter = require('./mdToHtmlConverter.js');
+const AbstractTextToHtmlConverter = require('./abstractTextToHtmlConverter.js');
 
 var inputText = 'This is the *first* paragraph.\n' +
     'This is the **second** paragraph.\n' +
     'This is the ~~third~~ paragraph.\n';
 
 console.log('Text after using basic converter:');
-var basicTextConverter = new BasicTextToHtmlConverter();
+var basicTextConverter = new AbstractTextToHtmlConverter();
 console.log(basicTextConverter.convertText(inputText));
 
 console.log('Text after using MD converter:');
-var mdToHtmlConverter = new MdToHtmlConverter();
+var mdToHtmlConverter = new AbstractTextToHtmlConverter();
 console.log(mdToHtmlConverter.convertText(inputText));
